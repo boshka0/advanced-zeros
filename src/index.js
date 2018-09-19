0734b9c9737e9e7c6784b9fa51b41987205bb549
boshka0 <nastyaboksha@gmail.com>
2018-09-19 18:05:43 +0300

    Finish getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
-  var multiplier = 2, simple = [], simple1 = [], balance, zeros = 0, zeroNum = [];
+  var multiplier = 2, simpleMultipliers = [], balance, zeroNum = [], obj = {}, zeros = [];
   while(multiplier <= base){
     balance = base % multiplier;
     if(!balance){
-      simple.push(multiplier);
+      simpleMultipliers.push(multiplier);
       base /= multiplier;
     }
     else multiplier++;
   } 
-  for(var i = 0; i < simple.length; i++){
+  for(var i = 0; i < simpleMultipliers.length; i++){
     var number1 = number;
     zeroNum[i] = 0;
     while(number1){
-      number1 = Math.floor(number1/simple[i]);
+      number1 = Math.floor(number1/simpleMultipliers[i]);
       zeroNum[i] += number1;
     }
   }
-  return Math.min.apply(null, zeroNum);
+  zeroNum.forEach(function(a){
+    if (obj[a] != undefined)
+        obj[a]++;
+    else
+        obj[a] = 1;
+  });
+  for(var key in obj){
+    zeros.push(Math.floor(+key/obj[key]));
+  }
+  return Math.min.apply(null, zeros);
 }
